c677b61db530f92bdde5dd15c1bc162a321f4e9a
97amarnathk <97amarnathk@MacBook-Pro.local>
2020-05-22 08:24:53 +0530

    plotter

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,5 +1,5 @@
-outfile = open("out_trial_clean.csv", "w")
-infile =  open("out_trial.csv", "r")
+outfile = open("../plotting/cleaned/cluster_out_fmmfft_32_15.txt", "w")
+infile =  open("../plotting/raw_data/cluster_out_fmmfft_32_15.txt", "r")
 
 for l in infile:
     if l[0] is not "[" and l[0].isdigit():
